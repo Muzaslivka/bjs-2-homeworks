dd0f281298dafd47e113e09f8cb7b49b844ccf0a
Margarita Dmitrieva <ms.dmitrieffa@gmail.com>
2026-03-26 13:15:48 +0300

    Task1, solveEquation realized

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+
+  const d = b ** 2 - 4 * a * c;
+
+  if (d < 0) {
+    return arr;
+  } else if (d === 0) {
+    const root = -b / (2 * a);
+    arr.push(root);
+  } else {
+    const sqrtD = Math.sqrt(d);
+    const root1 = (-b + sqrtD) / (2 * a);
+    const root2 = (-b - sqrtD) / (2 * a);
+    arr.push(root1, root2);
+  }
+
   return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+const roots = solveEquation(1, -3, 2);
+console.log(roots);
