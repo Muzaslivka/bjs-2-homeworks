6a6e62cc5bddcd55d7e9556bc50aea7bacb7a4b1
Margarita Dmitrieva <ms.dmitrieffa@gmail.com>
2026-04-10 16:28:56 +0300

    Task 2, the library is realized

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -62,3 +62,33 @@ class DetectiveBook extends Book {
     }
 }
 
+class Library {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) {
+        if (book.state > 30) {
+            this.books.push(book);
+        }
+    }
+
+    findBookBy(type, value) {
+        for (let book of this.books) {
+            if (book[type] === value) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    giveBookByName(bookName) {
+        const index = this.books.findIndex(book => book.name === bookName);
+        if (index !== -1) {
+            return this.books.splice(index, 1)[0];
+        }
+        return null;
+    }
+}
+
