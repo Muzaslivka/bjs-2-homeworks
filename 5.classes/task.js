82df631a52f4aee899e04f671ed979a80fd0543c
Margarita Dmitrieva <ms.dmitrieffa@gmail.com>
2026-04-10 16:39:36 +0300

    Task 3, the gradebook is realized

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -92,3 +92,45 @@ class Library {
     }
 }
 
+class Student {
+    constructor(name) {
+        this.name = name;
+        this.marks = {};
+    }
+
+    addMark(mark, subject) {
+        if (typeof mark !== 'number' || mark < 2 || mark > 5) {
+            return;
+        }
+
+        if (!this.marks.hasOwnProperty(subject)) {
+            this.marks[subject] = [];
+        }
+
+        this.marks[subject].push(mark);
+    }
+
+    getAverageBySubject(subject) {
+        if (!this.marks.hasOwnProperty(subject) || this.marks[subject].length === 0) {
+            return 0;
+        }
+
+        const sum = this.marks[subject].reduce((acc, val) => acc + val, 0);
+        return sum / this.marks[subject].length;
+    }
+
+    getAverage() {
+        const subjects = Object.keys(this.marks);
+        if (subjects.length === 0) {
+            return 0;
+        }
+
+        const totalSum = subjects.reduce((accSum, subject) => {
+            const avg = this.getAverageBySubject(subject);
+            return accSum + avg;
+        }, 0);
+
+        return totalSum / subjects.length;
+    }
+}
+
